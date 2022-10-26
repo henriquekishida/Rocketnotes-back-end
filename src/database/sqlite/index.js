27e8a016a7da32f87ec80f3f3814d3a090d64e3a
Henrique <henrique.kishida@hotmail.com>
2022-10-26 14:42:36 -0300

    first commit

---
+++ src/database/sqlite/index.js
@@ -0,0 +1,13 @@
+const sqlitet3 = require ("sqlite3")
+const sqlite = require ("sqlite")
+const path = require("path")
+
+async function sqliteConnection(){
+  const database = await sqlite.open({
+    filename: path.resolve(__dirname, "..", "database.db"),
+    driver: sqlitet3.Database
+  })
+  return database
+}
+
+module.exports = sqliteConnection
---
+++ src/server.js
@@ -0,0 +1,40 @@
+require("express-async-errors")
+require("dotenv/config")
+
+const migrationRun = require("./database/sqlite/migrations")
+const appError = require("./utils/appError")
+const uploadConfig = require("./configs/upload")
+
+const cors = require("cors")
+const express = require("express")
+const routes = require("./routes")
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
+
+app.use(routes)
+
+migrationRun()
+
+app.use((error, request, response, next) => {
+  if (error instanceof appError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    })
+  }
+
+  console.error(error)
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  })
+})
+
+const PORT = process.env.PORT || 3333
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
+
