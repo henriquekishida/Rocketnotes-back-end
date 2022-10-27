26e8647554fa41a8115b22e14aaf5cfa74676c7a
Henrique <henrique.kishida@hotmail.com>
2022-10-27 15:22:20 -0300

    Modified .env

--- src/server.js
+++ src/server.js
@@ -35,6 +35,6 @@ app.use((error, request, response, next) => {
   })
 })
 
-const PORT = process.env.SERVER_PORT || 3333
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`))
 
