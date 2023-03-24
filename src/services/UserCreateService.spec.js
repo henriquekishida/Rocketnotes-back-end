20c5c57dd8f7b7c051abd733ed07d695fdaa4e2f
Henrique <henrique.kishida@hotmail.com>
2023-03-24 17:05:53 -0300

    refactor: add beforeEach()

--- src/services/UserCreateService.spec.js
+++ src/services/UserCreateService.spec.js
@@ -4,6 +4,14 @@ const appError = require("../utils/appError")
 
 
 describe("UserCreateService", () => {
+  let userRepositoryInMemory = null
+  let userCreateService = null
+
+  beforeEach(() => {
+    userRepositoryInMemory = new UserRepositoryInMemory()
+    userCreateService = new UserCreateService(userRepositoryInMemory)
+  })
+  
   it("should create user", async () => {
     const user = {
       name: "User Test",
@@ -11,10 +19,7 @@ describe("UserCreateService", () => {
       password: "a"
     }
 
-    const userRepositoryInMemory = new UserRepositoryInMemory()
-    const userCreateService = new UserCreateService(userRepositoryInMemory)
     const userCreated = await userCreateService.execute(user)
-
     expect(userCreated).toHaveProperty("id")
   })
 
@@ -30,9 +35,6 @@ describe("UserCreateService", () => {
       email: "user@test.com",
       password: "abc"
     }
-
-    const userRepository = new UserRepositoryInMemory()
-    const userCreateService = new UserCreateService(userRepository)
 
     await userCreateService.execute(user1)
     await expect(userCreateService.execute(user2)).rejects.toEqual(new appError("This email is already in use."))
